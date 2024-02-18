4faf3952ca6fc9e709b2e8efb5e8ae24987cd693
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-02-18 12:50:03 +0800

    新增 onCreateClicked 監聽器, Create按鈕跳轉至create.html

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
 const DisplayMode = {
-  Block: "Block",
-  List: "List",
-};
+  Block: 'Block',
+  List: 'List',
+}
 
-const BASE_URL = "http://localhost:3000";
-const API_URL = `${BASE_URL}/articles`;
-const articleContainer = document.querySelector("#article-container");
-const articles = [];
-let displayMode = DisplayMode.List;
+const BASE_URL = 'http://localhost:3000'
+const API_URL = `${BASE_URL}/articles`
+const articleContainer = document.querySelector('#article-container')
+const articles = []
+let displayMode = DisplayMode.List
 
 // 參考用
-const POSTER_URL = `${BASE_URL}/posters`;
-const MOVIES_PER_PAGE = 12;
-const movies = [];
-const icons = document.querySelector(".icons");
-const searchForm = document.querySelector("#search-form");
-const searchInput = document.querySelector("#serach-input");
-const paginator = document.querySelector("#paginator");
-let currentMovies = [];
-let currentPage = 1;
+const POSTER_URL = `${BASE_URL}/posters`
+const MOVIES_PER_PAGE = 12
+const movies = []
+const icons = document.querySelector('.icons')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#serach-input')
+const paginator = document.querySelector('#paginator')
+const createArticle = document.querySelector('.create-article')
+let currentMovies = []
+let currentPage = 1
+
+createArticle.addEventListener('click', function onCreateClicked(event) {
+  if (event.target.matches('#createButton')) {
+    window.location.href = './create.html'
+  }
+})
 
 function getMoviesByPage(movies, page) {
-  const start = MOVIES_PER_PAGE * (page - 1);
-  return movies.slice(start, start + MOVIES_PER_PAGE);
+  const start = MOVIES_PER_PAGE * (page - 1)
+  return movies.slice(start, start + MOVIES_PER_PAGE)
 }
 
 // 渲染所有文章
 function renderArticles(articles) {
-  articleContainer.innerHTML = "";
-  let parent;
+  articleContainer.innerHTML = ''
+  let parent
   if (displayMode === DisplayMode.Block) {
-    parent = dataPanel;
+    parent = dataPanel
   } else {
-    parent = document.createElement("ul");
-    parent.classList.add("list-group", "col-sm-12", "mb-2");
-    articleContainer.appendChild(parent);
+    parent = document.createElement('ul')
+    parent.classList.add('list-group', 'col-sm-12', 'mb-2')
+    articleContainer.appendChild(parent)
   }
   articles.forEach((article) => {
-    renderArticle(parent, article);
-  });
+    renderArticle(parent, article)
+  })
 }
 
 // 渲染單篇文章
 function renderArticle(parent, article) {
-  let child;
+  let child
   if (displayMode === DisplayMode.Block) {
-    child = document.createElement("div");
-    child.classList.add("col-sm-3");
-    child.innerHTML = ``;
+    child = document.createElement('div')
+    child.classList.add('col-sm-3')
+    child.innerHTML = ``
   } else {
-    child = document.createElement("li");
-    child.classList.add(
-      "list-group-item",
-      "d-flex",
-      "justify-content-between",
-      "article"
-    );
-    let preview = article.content.substring(0, 20);
+    child = document.createElement('li')
+    child.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'article')
+    let preview = article.content.substring(0, 20)
     if (article.content.length > 20) {
-      preview += "...";
+      preview += '...'
     }
     child.innerHTML = `
     <div class="article-left">
@@ -86,43 +88,43 @@ function renderArticle(parent, article) {
           <i class="fa-solid fa-thumbs-down dislike"> 3</i>
           <i class="fa-solid fa-heart favorite"> 7</i>
       </div>
-    </div>`;
+    </div>`
   }
-  parent.appendChild(child);
+  parent.appendChild(child)
 }
 
 function showMovieModal(id) {
-  const modalTitle = document.querySelector("#movie-modal-title");
-  const modalImage = document.querySelector("#movie-modal-image");
-  const modalDate = document.querySelector("#movie-modal-date");
-  const modalDescription = document.querySelector("#movie-modal-description");
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
   axios.get(`${API_URL}/${id}`).then((response) => {
-    const data = response.data.results;
-    modalTitle.innerText = data.title;
-    modalDate.innerText = "Release date: " + data.release_date;
-    modalDescription.innerText = data.description;
-    modalImage.innerHTML = `<img src="${POSTER_URL}/${data.image}" alt="movie-poster" class="img-fluid">`;
-  });
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL}/${data.image}" alt="movie-poster" class="img-fluid">`
+  })
 }
 
 function addToFavorite(id) {
-  const favorites = JSON.parse(localStorage.getItem("favorite-movies")) ?? [];
-  const movie = movies.find((m) => m.id === id);
+  const favorites = JSON.parse(localStorage.getItem('favorite-movies')) ?? []
+  const movie = movies.find((m) => m.id === id)
   if (favorites.some((m) => m.id === id)) {
-    alert(`電影 '${movie.title}' 已收藏`);
+    alert(`電影 '${movie.title}' 已收藏`)
   } else {
-    favorites.push(movie);
-    localStorage.setItem("favorite-movies", JSON.stringify(favorites));
+    favorites.push(movie)
+    localStorage.setItem('favorite-movies', JSON.stringify(favorites))
   }
 }
 
 function setPaginator(amount) {
-  let len = Math.ceil(amount / MOVIES_PER_PAGE);
-  let pages = "";
+  let len = Math.ceil(amount / MOVIES_PER_PAGE)
+  let pages = ''
   for (let i = 1; i <= len; i++) {
-    pages += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+    pages += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`
   }
-  paginator.innerHTML = pages;
+  paginator.innerHTML = pages
 }
 
 function init() {
@@ -181,17 +183,17 @@ function init() {
   axios
     .get(API_URL)
     .then((response) => {
-      let datas = response.data;
-      articles.push(...datas);
+      let datas = response.data
+      articles.push(...datas)
       // currentMovies = movies;
       // setPaginator(movies.length);
       // currentPage = 1;
       // renderArticles(getMoviesByPage(movies, currentPage));
-      renderArticles(articles);
+      renderArticles(articles)
     })
     .catch((error) => {
-      console.log(error);
-    });
+      console.log(error)
+    })
 }
 
-init();
+init()
