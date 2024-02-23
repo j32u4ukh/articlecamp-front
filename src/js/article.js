01df0a361dfef5a5a9a5138ee47eed6b09819a45
ShangRuey <tom830522@gmail.com>
2024-02-23 20:51:51 +0800

    新增根據index.html 的cookie id,動態渲染article.html

--- src/js/article.js
+++ src/js/article.js
@@ -1,3 +1,19 @@
+const articleId = Number(getCookie("articleId"));
+const BASE_URL = "http://localhost:3000";
+const API_URL = `${BASE_URL}/articles/${articleId}`;
+const articleContent = document.querySelector(".article-content");
+
+function renderArticle(data) {
+  articleContent.innerHTML = `<h1 class="article-title">文章標題: ${data.title}</h1>
+          <h3 class="article-author">文章作者: ${data.author}</h3>
+          <div class="article-context">
+            文章內容:
+            <article>
+              ${data.content}
+            </article>
+          </div>`;
+}
+
 function getCookies() {
   const datas = document.cookie.split(";");
   let cookies = {};
@@ -29,5 +45,14 @@ function setCookie(key, value) {
   document.cookie = options.join(";");
 }
 
-const articleId = Number(getCookie("articleId"));
-console.log(`articleId: ${articleId}`);
+axios
+  .get(API_URL)
+  .then((response) => {
+    let data = response.data;
+    console.log(data);
+    console.log(typeof data);
+    renderArticle(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
