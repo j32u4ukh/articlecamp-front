8eee6e8b90f1321b09811d26e4de3b6e678f9031
j32u4ukh <j32u4ukh@gmail.com>
2024-02-23 22:58:03 +0800

    refactor: 調整格式與排版

--- src/js/article.js
+++ src/js/article.js
@@ -1,58 +1,58 @@
-const articleId = Number(getCookie("articleId"));
-const BASE_URL = "http://localhost:3000";
-const API_URL = `${BASE_URL}/articles/${articleId}`;
-const articleContent = document.querySelector(".article-content");
+const articleId = Number(getCookie('articleId'))
+const BASE_URL = 'http://localhost:3000'
+const API_URL = `${BASE_URL}/articles/${articleId}`
+const articleContent = document.querySelector('.article-content')
 
 function renderArticle(data) {
   articleContent.innerHTML = `<h1 class="article-title">文章標題: ${data.title}</h1>
-          <h3 class="article-author">文章作者: ${data.author}</h3>
-          <div class="article-context">
-            文章內容:
-            <article>
-              ${data.content}
-            </article>
-          </div>`;
+    <h3 class="article-author">文章作者: ${data.author}</h3>
+    <div class="article-context">
+      文章內容:
+      <article>
+        ${data.content}
+      </article>
+    </div>`
 }
 
 function getCookies() {
-  const datas = document.cookie.split(";");
-  let cookies = {};
-  const options = [];
+  const datas = document.cookie.split(';')
+  let cookies = {}
+  const options = []
   datas.forEach((data) => {
-    if (data.includes("=")) {
-      const [key, value] = data.split("=");
-      if (key === "data") {
-        cookies = JSON.parse(value);
+    if (data.includes('=')) {
+      const [key, value] = data.split('=')
+      if (key === 'data') {
+        cookies = JSON.parse(value)
       } else {
-        options.push(data);
+        options.push(data)
       }
     } else {
-      options.push(data);
+      options.push(data)
     }
-  });
-  return [cookies, options];
+  })
+  return [cookies, options]
 }
 
 function getCookie(key) {
-  const [cookies, _] = getCookies();
-  return cookies[key];
+  const [cookies, _] = getCookies()
+  return cookies[key]
 }
 
 function setCookie(key, value) {
-  const [cookies, options] = getCookies();
-  cookies[key] = value;
-  options.push(`data=${JSON.stringify(cookies)}`);
-  document.cookie = options.join(";");
+  const [cookies, options] = getCookies()
+  cookies[key] = value
+  options.push(`data=${JSON.stringify(cookies)}`)
+  document.cookie = options.join(';')
 }
 
 axios
   .get(API_URL)
   .then((response) => {
-    let data = response.data;
-    console.log(data);
-    console.log(typeof data);
-    renderArticle(data);
+    const data = response.data
+    // console.log(data)
+    // console.log(typeof data)
+    renderArticle(data)
   })
   .catch((error) => {
-    console.log(error);
-  });
+    console.log(error)
+  })
