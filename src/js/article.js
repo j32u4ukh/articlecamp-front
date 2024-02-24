4881ddc4579f746c5f73e4242bd3c4120609f36d
j32u4ukh <j32u4ukh@gmail.com>
2024-02-24 10:34:04 +0800

    feat: 將 cookie 和 query 參數整合到 utils.js

--- src/js/article.js
+++ src/js/article.js
@@ -3,25 +3,6 @@ const BASE_URL = 'http://localhost:3000'
 const API_URL = `${BASE_URL}/articles/${articleId}`
 const articleContent = document.querySelector('.article-content')
 
-let href = new URL(window.location.href)
-console.log(`window.location.href: ${window.location.href}`)
-
-// 使用 URL 對象創建 URLSearchParams 對象
-let urlParams = href.searchParams
-console.log(`urlParams: ${urlParams}`)
-
-// 獲取特定參數的值
-let parameterValue = urlParams.get('id')
-
-// 打印參數值
-console.log(`parameterValue: ${parameterValue}`)
-
-let local = localStorage.getItem('articleId')
-console.log(`local: ${local}`)
-
-let se = sessionStorage.getItem('articleId')
-console.log(`se: ${se}`)
-
 function renderArticle(data) {
   articleContent.innerHTML = `<h1 class="article-title">文章標題: ${data.title}</h1>
     <h3 class="article-author">文章作者: ${data.author}</h3>
@@ -33,43 +14,10 @@ function renderArticle(data) {
     </div>`
 }
 
-function getCookies() {
-  const datas = document.cookie.split(';')
-  let cookies = {}
-  const options = []
-  datas.forEach((data) => {
-    if (data.includes('=')) {
-      const [key, value] = data.split('=')
-      if (key === 'data') {
-        cookies = JSON.parse(value)
-      } else {
-        options.push(data)
-      }
-    } else {
-      options.push(data)
-    }
-  })
-  return [cookies, options]
-}
-
-function getCookie(key) {
-  const [cookies, _] = getCookies()
-  return cookies[key]
-}
-
-function setCookie(key, value) {
-  const [cookies, options] = getCookies()
-  cookies[key] = value
-  options.push(`data=${JSON.stringify(cookies)}`)
-  document.cookie = options.join(';')
-}
-
 axios
   .get(API_URL)
   .then((response) => {
     const data = response.data
-    // console.log(data)
-    // console.log(typeof data)
     renderArticle(data)
   })
   .catch((error) => {
