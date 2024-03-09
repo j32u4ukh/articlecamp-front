2f83ae8ba6d1e4159b2cb01eb2befdaf350f5a66
j32u4ukh <j32u4ukh@gmail.com>
2024-03-09 09:36:48 +0800

    feat: 讀寫數據相關函式抽出到 utils

---
+++ utils/model.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // 從檔案中讀取數據
+  read(file_path) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(file_path, 'utf8', (err, data) => {
+        if (err) {
+          reject(`讀取數據失敗, err: ${err}`)
+        }
+        try {
+          const results = JSON.parse(data)
+          resolve(results)
+        } catch (error) {
+          reject(`解析 Json 格式數據時發生錯誤, error: ${error}`)
+        }
+      })
+    })
+  },
+  // 將數據寫入檔案中
+  write(file_path, data) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(file_path, JSON.stringify(data), 'utf8', (err) => {
+        if (err) {
+          reject(reject(`寫出數據失敗, err: ${err}`))
+        } else {
+          resolve()
+        }
+      })
+    })
+  },
+}
