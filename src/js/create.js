8a170a1b8169495da2378573f9f1139c9ab3cd09
ShangRuey <tom830522@gmail.com>
2024-03-17 21:30:16 +0800

    feat: 針對create 頁面，新增 根據分類cookie 建立分類選項 ，更新API 參數

--- src/js/create.js
+++ src/js/create.js
@@ -5,17 +5,41 @@ const articleContext = document.querySelector('#article-context')
 const cancelButton = document.querySelector('.cancel-btn')
 const homeIcon = document.querySelector('.icon')
 const API_URL = `${BASE_URL}/articles/create`
+// 0317 取得下拉選單區塊
+const articleCategory = document.querySelector('.article-category')
+
+// 0317 根據 getCookie 的 category 數據， 動態新增新的選項並設定value屬性
+function renderCategory() {
+  //取得StringCookie 並轉換型態
+  const categoryArrayCookie = getCookie('categoryArrayCookie')
+  console.log(categoryArrayCookie)
+  const categoryCookie = JSON.parse(categoryArrayCookie)
+  console.log(categoryCookie)
+  //根據CookieArray長度 設定要建立幾個option 並給予值 跟文字顯示name
+  for (let count = 0; count < categoryCookie.length; count++) {
+    const category = categoryCookie[count]
+    const newOption = document.createElement('option')
+    newOption.textContent = category.name
+    newOption.setAttribute('value', category.id)
+    // 将新创建的 option 元素添加到 articleCategory 中
+    articleCategory.appendChild(newOption)
+    console.log(newOption.value)
+  }
+}
+renderCategory()
 
 // 新增文章後，送出 API 請求
-function createArticleAPI(author, title, content) {
+// 0317 新增category參數
+function createArticleAPI(author, title, content, category) {
   axios
     .post(API_URL, {
       author,
       title,
       content,
+      category,
     })
     .then((response) => {
-      // console.log(response)
+      //console.log(response)
     })
     .catch((error) => {
       console.log(error)
@@ -26,14 +50,17 @@ function createArticleAPI(author, title, content) {
 }
 
 // 新增文章按鈕，按下後，確認欄位填寫，新增文章
+// 0317 新增category 參數
 submitButton.addEventListener('click', function createArticleClicked(event) {
   const title = articleTitle.value.trim()
   const author = articleAuthor.value.trim()
   const content = articleContext.value.trim()
+  const category = articleCategory.value
   if (title.length === 0 || author.length === 0 || content.length === 0) {
     alert('文章標題、文章作者、文章內容皆不得空白!')
   } else {
-    createArticleAPI(author, title, content)
+    createArticleAPI(author, title, content, category)
+    console.log(category)
   }
 })
 
