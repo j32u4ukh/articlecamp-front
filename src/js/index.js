a0a738ecb4f535f7171716e7903beacade04b649
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-20 20:23:37 +0800

    task41: 使用IntersectionObserver, 串接文章列表

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,20 @@ const homeIcon = document.querySelector('.icon')
 const navbar = document.querySelector('.nav-bar')
 const API_URL = `${BASE_URL}/articles`
 const articles = []
+// 總文章篇數
+let total = 0
+// 取得文章位移值
+let offset = 0
+// 取得文章篇數
+const size = 10
 //////////無限下滑//////////
 // 頁面載入後渲染文章篇數及下滑後新增篇數(可任意調整)
 // document.body.scrollHeight > window.innerHeight
-const articleCount = 10 // 參考:VH為695時,最低值為6
+// const articleCount = 10 // 參考:VH為695時,最低值為6
 // 滑動觸發前的Y軸值
-let previousY = 0
+// let previousY = 0
 // 已渲染總篇數
-let scrollCount = articleCount
+// let scrollCount = articleCount
 //////////無限下滑//////////
 
 // 渲染所有文章
@@ -26,60 +32,86 @@ function renderArticles(articles) {
   articles.forEach((article) => {
     renderArticle(article)
   })
+  if (total > offset) {
+    const lastArticle = articleContainer.lastChild
+    // 設置IntersectionObserver
+    const observer = new IntersectionObserver(
+      (entry) => {
+        if (entry[0].isIntersecting) {
+          console.log('觸發成功: 渲染新文章')
+          console.log(`offset: ${offset}`)
+          console.log(`size: ${size}`)
+          // 關閉觀察
+          observer.unobserve(lastArticle)
+          // 發送請求
+          axios
+            .get(`${API_URL}?offset=${offset}&size=${size}`)
+            .then((response) => {
+              const data = response.data
+              const articles = data.articles
+              // 渲染新文章
+              renderArticles(articles)
+              // 更新位移值
+              offset += size
+            })
+            .catch((error) => {
+              console.error(error)
+            })
+        }
+      },
+      { threshold: 1 } // 預設為0,0是觀察對象上方,1是下方
+    )
+    // 啟動觀察
+    observer.observe(lastArticle)
+  }
 }
 
 // NOTE: 目前高度沒有考慮到 Header
 // TODO: 嘗試使用 IntersectionObserver 實作無限下滑
 // TODO: 每次到底部時，再觸發 axios 發送請求後面的文章數據
-window.addEventListener('scroll', () => {
-  // 總滾動高度(此高度比實際最高 Y 軸值多一個 VH 的值)
-  // document.documentElement.scrollHeight: 用於獲取當前文檔的根元素（即 <html> 元素）的整個高度。這個值包括了所有文檔內容的高度，即使該內容超出了當前可見區域，也會被計算在內。
-  const scrollHeight = document.documentElement.scrollHeight
-  // VH 值
-  // window.innerHeight: 用於獲取當前瀏覽器視窗的內部高度，即瀏覽器窗口中可見內容的高度，不包括工具欄、地址欄、滾動條等非內容區域的高度。
-  const innerHeight = window.innerHeight
-  const offset = 10
-  // 觸發點(減 offset 是為了避免 Y 軸值的誤差可能造成的錯誤)
-  const triggerPoint = scrollHeight - innerHeight - offset
-  // 當下 Y 軸值
-  // window.scrollY 和 window.pageYOffset 都是 JavaScript 中用於獲取當前頁面垂直方向上滾動的距離的屬性。它們兩者的作用是相同的，都用於獲取用戶在垂直方向上滾動的距離，即滾動條距離視窗頂部的距離。
-  const scrollY = window.scrollY || window.pageYOffset
-  // 總文章篇數
-  const articlesLength = articles.length
-  // 條件 1: 預防網頁刷新後沒有跑回原點(Y = 0)且誤觸新增新文章篇數的渲染
-  // 如刷新後 scrollbar 介於觸發點之下, 且使用者向上滑動, 新文章篇數會被渲染
-  // 條件 2: 當下 Y 軸值大於觸發點
-  // 條件 3: 欲渲染篇數小於總文章數(避免無謂觸發)
-  if (
-    scrollY > previousY &&
-    scrollY > triggerPoint &&
-    scrollCount < articlesLength
-  ) {
-    console.log(`新增前篇數: ${scrollCount}`)
-    // 渲染文章篇數調整, 如剩餘渲染篇數較少, 只多渲染該數量
-    scrollCount =
-      articlesLength - scrollCount > articleCount
-        ? (scrollCount += articleCount)
-        : articlesLength
+// window.addEventListener('scroll', () => {
+//   // 總滾動高度(此高度比實際最高 Y 軸值多一個 VH 的值)
+//   // document.documentElement.scrollHeight: 用於獲取當前文檔的根元素（即 <html> 元素）的整個高度。這個值包括了所有文檔內容的高度，即使該內容超出了當前可見區域，也會被計算在內。
+//   const scrollHeight = document.documentElement.scrollHeight
+//   // VH 值
+//   // window.innerHeight: 用於獲取當前瀏覽器視窗的內部高度，即瀏覽器窗口中可見內容的高度，不包括工具欄、地址欄、滾動條等非內容區域的高度。
+//   const innerHeight = window.innerHeight
+//   const offset = 10
+//   // 觸發點(減 offset 是為了避免 Y 軸值的誤差可能造成的錯誤)
+//   const triggerPoint = scrollHeight - innerHeight - offset
+//   // 當下 Y 軸值
+//   // window.scrollY 和 window.pageYOffset 都是 JavaScript 中用於獲取當前頁面垂直方向上滾動的距離的屬性。它們兩者的作用是相同的，都用於獲取用戶在垂直方向上滾動的距離，即滾動條距離視窗頂部的距離。
+//   const scrollY = window.scrollY || window.pageYOffset
+//   // 總文章篇數
+//   const articlesLength = articles.length
+//   // 條件 1: 預防網頁刷新後沒有跑回原點(Y = 0)且誤觸新增新文章篇數的渲染
+//   // 如刷新後 scrollbar 介於觸發點之下, 且使用者向上滑動, 新文章篇數會被渲染
+//   // 條件 2: 當下 Y 軸值大於觸發點
+//   // 條件 3: 欲渲染篇數小於總文章數(避免無謂觸發)
+//   if (
+//     scrollY > previousY &&
+//     scrollY > triggerPoint &&
+//     scrollCount < articlesLength
+//   ) {
+//     console.log(`新增前篇數: ${scrollCount}`)
+//     // 渲染文章篇數調整, 如剩餘渲染篇數較少, 只多渲染該數量
+//     scrollCount =
+//       articlesLength - scrollCount > articleCount
+//         ? (scrollCount += articleCount)
+//         : articlesLength
 
-    console.log(`新增後篇數: ${scrollCount}`)
-    renderArticles(articles.slice(scrollCount, scrollCount + articleCount))
-    console.log(`From: ${scrollCount}, To: ${scrollCount + articleCount}`)
-  }
-  // scroll 事件觸發前的 Y 軸值
-  previousY = window.scrollY || window.pageYOffset
-})
+//     console.log(`新增後篇數: ${scrollCount}`)
+//     renderArticles(articles.slice(scrollCount, scrollCount + articleCount))
+//     console.log(`From: ${scrollCount}, To: ${scrollCount + articleCount}`)
+//   }
+//   // scroll 事件觸發前的 Y 軸值
+//   previousY = window.scrollY || window.pageYOffset
+// })
 
 // 渲染單篇文章
 function renderArticle(article) {
   const child = document.createElement('li')
-  child.classList.add(
-    'list-group-item',
-    'd-flex',
-    'justify-content-between',
-    'article',
-    'c-border'
-  )
+  child.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'article', 'c-border')
   let preview = article.content.substring(0, 20)
   if (article.content.length > 20) {
     preview += '...'
@@ -186,7 +218,7 @@ function setCategoryCookie() {
       .then((response) => {
         let datas = response.data
         articles.splice(0, articles.length)
-        articles.push(...datas)
+        articles.push(...datas.articles)
         renderArticles(articles)
       })
       .catch((error) => {
@@ -203,9 +235,15 @@ function setCategoryCookie() {
     .then((response) => {
       let datas = response.data
       console.log(datas)
-      articles.push(...datas)
+      console.log(`offset: ${datas.offset}`)
+      console.log(`size: ${datas.size}`)
+      // 總文章篇數
+      total = datas.total
+      // 更新位移值
+      offset = datas.size
+      articles.push(...datas.articles)
       // 頁面載入後渲染特定文章篇數
-      renderArticles(articles.slice(0, articleCount))
+      renderArticles(articles)
     })
     .catch((error) => {
       console.log(error)
