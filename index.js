585afae9f2a8a764ed4842b937415f39f098276a
j32u4ukh <j32u4ukh@gmail.com>
2024-03-24 08:32:11 +0800

    refactor: 移除無用資源

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const PORT = 3000
 
 // 使用 cors 中間件
 app.use(cors())
 
-// app.use(express.static('public'))
+// 告訴 Express 應用程式要使用 express.json() 中間件來解析請求主體中的 JSON 格式資料
 app.use(express.json())
 
 // 引用路由器
@@ -15,6 +15,6 @@ const router = require('./routes')
 // 將 request 導入路由器
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`express server is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`express server is running on http://localhost:${PORT}`)
 })
