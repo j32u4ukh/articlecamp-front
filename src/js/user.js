ed021894c6fe639bb21976ca5d6822cbf1ee9403
louis93507@gmail.com <louis93507@gmail.com>
2024-04-05 02:57:49 +0800

    修正followBtn

--- src/js/user.js
+++ src/js/user.js
@@ -1,29 +1,44 @@
+// const User = require('./UserModel')
+
 const userListContainer = document.querySelector('.user-list-container')
 const userContainer = document.querySelector('.user-container')
 const userImage = document.querySelector('.user-image')
 const userName = document.querySelector('.user-name')
-const followBtn = document.querySelector('.follow-btn')
+const followBtn = document.querySelectorAll('.follow-btn')
+const homeIcon = document.querySelector('.icon')
+const USERLIST_URL = `${BASE_URL}/users`
 
-const API_URL = `${BASE_URL}/users/:3`
+    ; (function init() {
+        homeIcon.addEventListener('click', (e) => {
+            window.location.href = './index.html'
+        })
+        followBtn.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                const target = e.target
+                if (target.matches('.follow-btn')) {
+                    const btnId = target.id
+                    const isfollowed = target.classList.toggle('followed')
+                    target.textContent = isfollowed ? 'Followed' : 'Follow'
+                    console.log(`ID:${btnId}`)
+                }
+            })
+        })
+    })()
 
-userContainer.addEventListener('click', (e) => {
-    const target = e.target
-    console.log(e)
-    if (target === followBtn) {
-        let isfollowed = followBtn.classList.toggle('followed')
-        followBtn.textContent = isfollowed ? 'Followed' : 'Follow'
-        console.log(isfollowed)
-    }
+
+
+const token = 2
+document.addEventListener('DOMContentLoaded', function () {
+    axios.get(USERLIST_URL, {
+        headers: {
+            token: token
+        },
+    })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 })
 
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     axios.get(API_URL)
-//         .then((res) => {
-
-//             console.log(res)
-//         })
-//         .catch((err) => {
-//             console.error(err)
-//         })
-// })
