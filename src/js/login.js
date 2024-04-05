22094c12058f8c1a3d1599ae8307c55a3d1c6239
j32u4ukh <j32u4ukh@gmail.com>
2024-04-05 23:12:17 +0800

    refactor: 調整 login.js & register.js 寫法

--- src/js/login.js
+++ src/js/login.js
@@ -1,18 +1,23 @@
-const API_URL = `${BASE_URL}/login`
 const loginSubmit = document.querySelector('#loginSubmit')
 const loginErrorMsg = document.querySelector('#loginErrorMsg')
+const emailInput = document.getElementById('email')
+const passwordInput = document.getElementById('password')
+const API_URL = `${BASE_URL}/login`
 
-//登入submit 監聽器
+// 登入 submit 監聽器
 loginSubmit.addEventListener('click', (event) => {
   event.preventDefault()
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
+  // NOTE: 不要重複取得 HTML 的元素，在最上方取得一次後重複使用!!
+  // const email = document.getElementById('email').value
+  // const password = document.getElementById('password').value
+  const email = emailInput.value.trim()
+  const password = passwordInput.value.trim()
   loginErrorMsg.textContent = ''
   axios
     .post(`${API_URL}`, { email: email, password: password })
     .then((response) => {
+      // TODO: 紀錄返回的 JWT
       window.location.href = 'articles.html'
-      console.log('登入成功')
     })
     .catch((error) => {
       const errorMsg = error.response.data.msg
--- src/js/register.js
+++ src/js/register.js
@@ -1,15 +1,25 @@
-const API_URL = `${BASE_URL}/register`
-
 const registerSubmit = document.querySelector('#registerSubmit')
 const registerErrorMsg = document.querySelector('#registerErrorMsg')
+const nameInput = document.getElementById('username')
+const emailInput = document.getElementById('email')
+const passwordInput = document.getElementById('password')
+const repasswordInput = document.getElementById('repassword')
+const API_URL = `${BASE_URL}/register`
 
-//註冊submit監聽器
+// 註冊 submit 監聽器
 registerSubmit.addEventListener('click', (event) => {
   event.preventDefault()
-  const username = document.getElementById('username').value
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
-  const repassword = document.getElementById('repassword').value
+  // NOTE: 不要重複取得 HTML 的元素，在最上方取得一次後重複使用!!
+  // const username = document.getElementById('username').value
+  // const email = document.getElementById('email').value
+  // const password = document.getElementById('password').value
+  // const repassword = document.getElementById('repassword').value
+  const username = nameInput.value.trim()
+  const email = emailInput.value.trim()
+  const password = passwordInput.value.trim()
+  const repassword = repasswordInput.value.trim()
+  registerErrorMsg.textContent = ''
+
   //密碼 與 確認密碼 需相同
   if (password === repassword) {
     axios
