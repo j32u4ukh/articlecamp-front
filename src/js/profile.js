a45cef398046b593ab326309f22ee63f45a1d63a
j32u4ukh <j32u4ukh@gmail.com>
2024-04-05 23:14:35 +0800

    refactor: 調整 profile.js 寫法

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,7 +1,8 @@
-const navbar = document.querySelector('.nav-bar')
 const userid = document.querySelector('#user-id')
 const cancelbtn = document.querySelector('#cancel-btn')
 const homeIcon = document.querySelector('.icon')
+const nameInput = document.querySelector('#user-name input')
+const emailInput = document.querySelector('#email input')
 
 // 獲取原始的用戶名和 email
 const originalUsername = 'UserName' // 替換為載入時獲取的原始用戶名
@@ -12,8 +13,8 @@ function renderUserId() {
   userid.textContent = `User ID: ${userId}`
 
   // 將用戶名和 email 載入
-  document.querySelector('#user-name input').value = originalUsername
-  document.querySelector('#email input').value = originalEmail
+  nameInput.value = originalUsername
+  emailInput.value = originalEmail
 }
 
 ;(function init() {
@@ -21,23 +22,11 @@ function renderUserId() {
     window.location.href = './index.html'
   })
 
-  // // 監聽 navbar
-  // navbar.addEventListener('click', function onNavbarClicked(event) {
-  //   // NOTE: target 會是實際被點擊的元素，而非外層的 div 標籤
-  //   const target = event.target
-  //   console.log(target)
-  //   console.log(target.classList)
-  //   console.log('icon => ' + target.classList.contains('icon'))
-  //   console.log(
-  //     'profile-picture => ' + target.classList.contains('profile-picture')
-  //   )
-  // })
-
   // 監聽 cancelbtn
   cancelbtn.addEventListener('click', function onCancelBtnClicked() {
     // 將用戶名和 email 載入原始值
-    document.querySelector('#user-name input').value = originalUsername
-    document.querySelector('#email input').value = originalEmail
+    nameInput.value = originalUsername
+    emailInput.value = originalEmail
   })
 
   renderUserId()
