289259b07dc25688148d3fe8edd3ec3e5299e175
ShangRuey <tom830522@gmail.com>
2024-04-06 23:52:09 +0800

    feat: 針對 login 頁面，登入成功將錯誤訊息欄清空

--- src/js/login.js
+++ src/js/login.js
@@ -4,14 +4,13 @@ const loginErrorMsg = document.querySelector('#loginErrorMsg')
 
 //登入submit 監聽器
 loginSubmit.addEventListener('click', (event) => {
-  event.preventDefault()
   const email = document.getElementById('email').value
   const password = document.getElementById('password').value
-  loginErrorMsg.textContent = ''
   axios
     .post(`${API_URL}`, { email: email, password: password })
     .then((response) => {
-      window.location.href = 'articles.html'
+      loginErrorMsg.textContent = ''
+      /*window.location.href = 'articles.html'*/
       console.log('登入成功')
     })
     .catch((error) => {
