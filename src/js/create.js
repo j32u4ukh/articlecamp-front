638daa896fd216dbbe3ddec3f8086562b83d30c0
ShangRuey <tom830522@gmail.com>
2024-04-11 23:47:47 +0800

    feat:針對create.js ，刪除 author 請求參數

--- src/js/create.js
+++ src/js/create.js
@@ -1,6 +1,5 @@
 const submitButton = document.querySelector('.submit-btn')
 const articleTitle = document.querySelector('#article-title')
-const articleAuthor = document.querySelector('#article-author')
 const articleContext = document.querySelector('#article-context')
 const cancelButton = document.querySelector('.cancel-btn')
 const homeIcon = document.querySelector('.icon')
@@ -32,12 +31,11 @@ function renderCategory() {
 }
 
 // 新增文章後，送出 API 請求
-function createArticleAPI(author, title, content, category) {
+function createArticleAPI(title, content, category) {
   axios
     .post(
       API_URL,
       {
-        author,
         title,
         content,
         category,
@@ -59,14 +57,13 @@ function createArticleAPI(author, title, content, category) {
   // 新增文章按鈕，按下後，確認欄位填寫，新增文章
   submitButton.addEventListener('click', function createArticleClicked(event) {
     const title = articleTitle.value.trim()
-    const author = articleAuthor.value.trim()
     const content = articleContext.value.trim()
     const category = articleCategory.value
     // console.log(`category: ${category}`)
-    if (title.length === 0 || author.length === 0 || content.length === 0) {
-      alert('文章標題、文章作者、文章內容皆不得空白!')
+    if (title.length === 0 || content.length === 0) {
+      alert('文章標題、文章內容皆不得空白!')
     } else {
-      createArticleAPI(author, title, content, category)
+      createArticleAPI(title, content, category)
     }
   })
 
