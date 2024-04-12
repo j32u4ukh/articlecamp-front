3c8aac613db4d6cba3b8395190aa34efcbb8948e
j32u4ukh <j32u4ukh@gmail.com>
2024-04-12 20:39:11 +0800

    feat: 置換成 React 版本

---
+++ resources/src/js/utils.js
@@ -0,0 +1,40 @@
+const VERSION = 2;
+const PORT = 3000;
+const BASE_URL = `http://localhost:${PORT}/v${VERSION}`;
+
+class Cookie {
+  constructor(options) {
+    this.options = options;
+  }
+  // 根據 key, value 設置 cookie 的內容
+  set(key, value) {
+    const cookies = this._get();
+    cookies[key] = value;
+    const options = [`data=${JSON.stringify(cookies)}`];
+    this.options.forEach((option) => {
+      options.push(option);
+    });
+    document.cookie = options.join("; ");
+  }
+  // 根據 key 取得 cookie 的內容
+  get(key) {
+    const cookies = this._get();
+    return cookies[key];
+  }
+  // 取得 cookie 的完整資料結構
+  _get() {
+    const datas = document.cookie.split(";");
+    const len = datas.length;
+    let data;
+    for (let i = 0; i < len; i++) {
+      data = datas[i].trim();
+      const [k, v] = data.split("=");
+      if (k === "data") {
+        return JSON.parse(v);
+      }
+    }
+    return {};
+  }
+}
+
+const COOKIE = new Cookie(["SameSite=None", "Secure"]);
