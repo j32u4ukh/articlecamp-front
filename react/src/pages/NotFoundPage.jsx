c4945a7e9fc5b3858f7db6c79bf3dd9d7289cba9
j32u4ukh <j32u4ukh@gmail.com>
2024-04-13 15:44:44 +0800

    feat: 垂直置中布局

--- react/src/pages/NotFoundPage.jsx
+++ react/src/pages/NotFoundPage.jsx
@@ -1,6 +1,8 @@
+import CommonStyle from "./common.module.css"
+
 export default function NotFoundPage(){
     return (
-        <div>
+        <div className={`${CommonStyle["vertical-center-layout"]}`}>
             <h1>NotFound</h1>
             <img src="/images/icons8-not-found-100.png"></img>
         </div>
