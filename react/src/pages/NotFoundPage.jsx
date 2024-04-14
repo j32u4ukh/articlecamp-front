dd11a12c79bd9a8b1fefaa6127a7c1a048bb6ac0
j32u4ukh <j32u4ukh@gmail.com>
2024-04-14 20:30:17 +0800

    feat: 上層容器套用 container 的樣式，修正容器中的大小設置

--- react/src/pages/NotFoundPage.jsx
+++ react/src/pages/NotFoundPage.jsx
@@ -2,11 +2,11 @@ import CommonStyle from "./common.module.css"
 
 export default function NotFoundPage(){
     return (
-        <div className={`${CommonStyle["vertical-center-layout"]}`}>
+        <div className={`${CommonStyle["vertical-center-layout"]} ${CommonStyle["container"]}`}>
             <h1 style={{margin: "20px auto"}}>404 Not Found</h1>
             {/* aspect-ratio:設置元素的寬高比 */}
             <img 
-                style={{height: "40%", width: "40%", maxWidth: "62.5%", aspectRatio: "1/1", margin: "20px auto"}}
+                style={{height: "62.5%", width: "62.5%", maxWidth: "100%", aspectRatio: "1/1", margin: "20px auto"}}
                 src="/images/icons8-not-found-100.png"></img>
         </div>
     );
