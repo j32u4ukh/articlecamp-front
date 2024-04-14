5eaefb74b53eff48c0f4ea11fb2b75d38f60d562
j32u4ukh <j32u4ukh@gmail.com>
2024-04-14 20:27:13 +0800

    feat: 將標題與圖片套用垂直置中的布局

--- react/src/pages/NotFoundPage.jsx
+++ react/src/pages/NotFoundPage.jsx
@@ -3,8 +3,11 @@ import CommonStyle from "./common.module.css"
 export default function NotFoundPage(){
     return (
         <div className={`${CommonStyle["vertical-center-layout"]}`}>
-            <h1>NotFound</h1>
-            <img src="/images/icons8-not-found-100.png"></img>
+            <h1 style={{margin: "20px auto"}}>404 Not Found</h1>
+            {/* aspect-ratio:設置元素的寬高比 */}
+            <img 
+                style={{height: "40%", width: "40%", maxWidth: "62.5%", aspectRatio: "1/1", margin: "20px auto"}}
+                src="/images/icons8-not-found-100.png"></img>
         </div>
     );
 };
