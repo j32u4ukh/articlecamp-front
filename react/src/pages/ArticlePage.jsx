d987fceda7717b63d4787cbad69475d075da6f2b
ShangRuey <tom830522@gmail.com>
2024-04-19 18:01:11 +0800

    refactor:以先前article.html 頁面，重構成ArticlePage.jsx

--- react/src/pages/ArticlePage.jsx
+++ react/src/pages/ArticlePage.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import styles from './articlepage.module.css';
+import AlexImage from '../../../src/data/Alex.png';
 
 export default function ArticlePage(){
     const navigate = useNavigate();
@@ -16,6 +18,45 @@ export default function ArticlePage(){
     }, []);
 
     return (
-        <h1>Article Page # {articleId}</h1>
+        <>
+            <button id={styles.editButton}>
+                <i className="fa-solid fa-pen-to-square" style={{ fontSize: '30px' }}></i>
+            </button>
+
+            <section id={styles.articleContainer}>
+                
+                <div className={styles.articleContent}>
+                    <h1>Article Page # {articleId}</h1>
+                    <h1 className={styles.articleTitle}>文章標題:</h1>
+                    <h3>文章作者:</h3>
+                    <h3>文章分類:</h3>
+                    <div className={styles.articleContext}>
+                        文章內容:
+                        <article></article>
+                    </div>
+                </div>
+            </section>
+
+            {/* 留言區 */}
+            <div id={styles.commentsContainer}>
+                <div id={styles.commenter}>
+                    <div className={styles.commenterImg}>
+                        <img src={AlexImage} />
+                    </div>
+                    <div className={styles.commenterName}></div>
+                </div>
+
+                <form id={styles.commentForm}>
+                    <textarea id={styles.comment} placeholder="請留下您的評論" required>
+                    </textarea>
+                </form>
+                <div className={styles.commentControls}>
+                    <button>留言</button>
+                    <button>取消</button>
+                </div>
+            </div>
+            {/* 歷史留言區 (還要放留言者頭像及名字) */}
+            <div className={styles.commentList}></div>
+        </>
     );
 }
