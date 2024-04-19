96de6b27ad7793f60d4a77daaee2ae61e73e7617
ShangRuey <tom830522@gmail.com>
2024-04-19 16:16:19 +0800

    refactor: 將先前的edit.html 重構為 EditPage.jsx

--- react/src/pages/EditPage.jsx
+++ react/src/pages/EditPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import styles from './editpage.module.css';
 
 export default function EditPage(){
     const navigate = useNavigate();
@@ -17,7 +18,37 @@ export default function EditPage(){
 
     return (
         <>
-            <h1>Edit Page # {articleId}</h1>
+            <div className={styles.articleContent}>
+                <h1>Edit Page # {articleId}</h1>
+                <label className={styles.articleTitle}>文章標題: 
+                    <input type="text" value="" id="article-title" />
+                </label>
+                <br />
+                <label>文章類別: </label>
+                <select className={styles.articleCategory}>
+                    <option selected>請選擇文章類別</option>
+                    <option>類別1</option>
+                    <option>類別2</option>
+                    <option>類別3</option>
+                    <option>類別4</option>
+                    <option>類別5</option>
+                    <option>類別6</option>
+                    <option>類別7</option>
+                </select>
+                <div className={styles.articleContext}>
+                    文章內容:
+                    <article>
+                    <textarea
+                        id="article-context"
+                        cols="50"
+                        rows="4">
+                        預設的文章內容
+                    </textarea>
+                    </article>
+                </div>
+                 <button className={styles.updateBtn}>Cancel</button>
+                 <button className={styles.cancelBtn}>Submit</button>
+            </div>
         </>
     );
 }
