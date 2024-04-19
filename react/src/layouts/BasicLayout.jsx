bb9da8fe806e99628e0d19e3a6156b0cd2e71d17
ShangRuey <tom830522@gmail.com>
2024-04-19 16:08:23 +0800

    style: 針對 BasicLayout 的 main 標籤 ，以先前CSS設定 補充

--- react/src/layouts/BasicLayout.jsx
+++ react/src/layouts/BasicLayout.jsx
@@ -1,11 +1,18 @@
 // import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+const style = {
+  height: '85vh',
+  padding: '20px',
+  'padding-bottom': '100px',
+  position: 'relative'
+};
+
 export default function BasicLayout({ children }) {
     return (
       <>
         <Header/>
-        <main style={{height: '85vh'}}>{children}</main>
+        <main style={style}>{children}</main>
         {/* <Footer/> */}
       </>
     );
