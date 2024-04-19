67d52e1b7dc2f6fa68903e6bae9cd450e5d1562a
ShangRuey <tom830522@gmail.com>
2024-04-19 16:15:51 +0800

    refactor: 將先前的Create.html 重構為 CreatePage.jsx

--- react/src/pages/CreatePage.jsx
+++ react/src/pages/CreatePage.jsx
@@ -1,7 +1,38 @@
+import styles from './createpage.module.css';
+
 export default function CreatePage(){
     return (
         <>
-            <h1>Create Page</h1>
+          <div className={styles.articleContent}>
+              <label className={styles.articleTitle}>文章標題: 
+                  <input type="text" value="" id="article-title" />
+              </label>
+              <br />
+              <label>文章類別: </label>
+              <select className={styles.articleCategory}>
+                <option selected>請選擇文章類別</option>
+                <option>類別1</option>
+                <option>類別2</option>
+                <option>類別3</option>
+                <option>類別4</option>
+                <option>類別5</option>
+                <option>類別6</option>
+                <option>類別7</option>
+              </select>
+              <div className={styles.articleContext}>
+                文章內容:
+                <article>
+                  <textarea
+                  id="article-context"
+                  cols="50"
+                  rows="4">
+                    預設的文章內容
+                  </textarea>
+                </article>
+              </div>
+              <button className={styles.updateBtn}>Cancel</button>
+              <button className={styles.cancelBtn}>Submit</button>
+          </div>
         </>
     );
 }
