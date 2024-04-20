27811c7082e89d3393531d823393092ca5a09810
ShangRuey <tom830522@gmail.com>
2024-04-21 00:02:06 +0800

    style: 針對 BasicLayout 刪除自行新增的 CSS

--- react/src/layouts/BasicLayout.jsx
+++ react/src/layouts/BasicLayout.jsx
@@ -1,18 +1,11 @@
 // import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-const style = {
-  height: '85vh',
-  padding: '20px',
-  'padding-bottom': '100px',
-  position: 'relative'
-};
-
 export default function BasicLayout({ children }) {
     return (
       <>
         <Header/>
-        <main style={style}>{children}</main>
+        <main style={{height:'85vh'}}>{children}</main>
         {/* <Footer/> */}
       </>
     );
