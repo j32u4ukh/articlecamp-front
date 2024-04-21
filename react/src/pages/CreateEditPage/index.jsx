39edce158c226a8c2c5596725fca1ee4fd704b98
j32u4ukh <j32u4ukh@gmail.com>
2024-04-21 20:32:50 +0800

    feat: 調整按鈕排版 & 移除多餘標籤或文字

--- react/src/pages/CreateEditPage/index.jsx
+++ react/src/pages/CreateEditPage/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { capitalizeFirstLetter } from '../../utils'
-import styles from './styles.module.css'
+import Styles from './styles.module.css'
 
 export default function CreateEditPage(props) {
   const type = capitalizeFirstLetter(props.type)
@@ -11,14 +11,15 @@ export default function CreateEditPage(props) {
   const params = useParams()
   const navigate = useNavigate()
   
-  function onChangeHandler(e) {
+  function onContentChanged(e) {
     setContent(e.target.value)
   }
 
-  function onTitleHandler(e) {
+  function onTitleChanged(e) {
     setTitle(e.target.value)
   }
 
+  // TODO: 確認 if (isEdit) 在 useEffect 外面和在 useEffect 裡面有何差別
   if (isEdit) {
     useEffect(() => {
       // 檢查 id 是否為數字
@@ -30,38 +31,39 @@ export default function CreateEditPage(props) {
   }
   
   return (
-    <>
-            <div className={styles.content}>
-                <h1>{type} Page {isEdit && `#${params.id}`}</h1>
-                <label className={styles.articleTitle}>文章標題: 
-                    <input type="text" value={title} onChange={onTitleHandler} id="article-title" />
-                </label>
-                <br />
-                <label>文章類別: </label>
-                <select className={styles.category}>
-                    <option value={content} selected>{isEdit ? `編輯的分類` : `預設的文章類別`}</option>
-                    <option>類別1</option>
-                    <option>類別2</option>
-                    <option>類別3</option>
-                    <option>類別4</option>
-                    <option>類別5</option>
-                    <option>類別6</option>
-                    <option>類別7</option>
-                </select>
-                <div className={styles.context}>
-                    文章內容:
-                    <article>
-                    <textarea
-                        id="article-context"
-                        cols="50"
-                        rows="4"
-                        value={content} onChange={onChangeHandler}>
-                    </textarea>
-                    </article>
-                </div>
-                 <button className={styles.updateBtn}>Cancel</button>
-                 <button className={styles.cancelBtn}>Submit</button>
-            </div>
-        </>
+    <div className={Styles.content}>
+        <h1>{type} Page {isEdit && `#${params.id}`}</h1>
+        <label className={Styles.articleTitle}>文章標題: 
+            <input type="text" value={title} onChange={onTitleChanged} id="article-title" />
+        </label>
+        <br />
+        <label>文章類別: </label>
+        <select className={Styles.category}>
+            {/* <option value={content} selected>{isEdit ? `編輯的分類` : `預設的文章類別`}</option> */}
+            <option value="0" selected>無分類</option>
+            <option>類別1</option>
+            <option>類別2</option>
+            <option>類別3</option>
+            <option>類別4</option>
+            <option>類別5</option>
+            <option>類別6</option>
+            <option>類別7</option>
+        </select>
+        <div className={Styles.context}>
+            文章內容:
+            <article>
+            <textarea
+                id="article-context"
+                cols="50"
+                rows="4"
+                value={content} onChange={onContentChanged}>
+            </textarea>
+            </article>
+        </div>
+        <div className={Styles.applyBtns}>          
+          <button className={Styles.updateBtn}>Cancel</button>
+          <button className={Styles.cancelBtn}>Submit</button>
+        </div>
+    </div>
   )
 }
