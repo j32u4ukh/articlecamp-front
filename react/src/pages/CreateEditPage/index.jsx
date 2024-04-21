6bb393a4ada7cec1ea8558228cd1616a32f11aa6
j32u4ukh <j32u4ukh@gmail.com>
2024-04-21 20:39:35 +0800

    feat: CreateEditPage 根據 type 進行不同前處理

--- react/src/pages/CreateEditPage/index.jsx
+++ react/src/pages/CreateEditPage/index.jsx
@@ -5,11 +5,11 @@ import Styles from './styles.module.css'
 
 export default function CreateEditPage(props) {
   const type = capitalizeFirstLetter(props.type)
-  const isEdit = type === 'Edit'
-  const [title, setTitle] = useState(isEdit ? '編輯標題' : '')
-  const [content, setContent] = useState(isEdit ? '編輯' : '')
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
   const params = useParams()
   const navigate = useNavigate()
+  const isEdit = type === 'Edit'
   
   function onContentChanged(e) {
     setContent(e.target.value)
@@ -19,15 +19,22 @@ export default function CreateEditPage(props) {
     setTitle(e.target.value)
   }
 
-  // TODO: 確認 if (isEdit) 在 useEffect 外面和在 useEffect 裡面有何差別
-  if (isEdit) {
-    useEffect(() => {
-      // 檢查 id 是否為數字
-      if (isNaN(Number(params.id))) {
-        // 如果 id 不是數字，使用 history.replace() 重定向到 NotFoundPage
-        navigate('/NotFound', { replace: true })
-      }
-    }, [])
+  // 根據 type 進行不同前處理
+  switch(type){
+    case 'Edit':
+      // TODO: 呼叫 API 取的文章內容
+      useEffect(() => {
+        // 檢查 id 是否為數字
+        if (isNaN(Number(params.id))) {
+          // 如果 id 不是數字，使用 history.replace() 重定向到 NotFoundPage
+          navigate('/NotFound', { replace: true })
+        }
+      }, [])
+      break
+    case 'Create':
+      break
+    default:
+      return navigate('/NotFound', { replace: true })
   }
   
   return (
@@ -40,6 +47,7 @@ export default function CreateEditPage(props) {
         <label>文章類別: </label>
         <select className={Styles.category}>
             {/* <option value={content} selected>{isEdit ? `編輯的分類` : `預設的文章類別`}</option> */}
+            {/* TODO: 動態渲染下拉選單 */}
             <option value="0" selected>無分類</option>
             <option>類別1</option>
             <option>類別2</option>
