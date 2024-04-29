bbcd36d49dffcbf4c10cbbcd741b4b3cd0fe0557
j32u4ukh <j32u4ukh@gmail.com>
2024-04-29 22:07:52 +0800

    feat: 在用戶列表中放入假的 FollowUserItem

--- react/src/pages/UsersPage/index.jsx
+++ react/src/pages/UsersPage/index.jsx
@@ -1,18 +1,42 @@
-import BasicLayout from '../../layouts/BasicLayout';
-import PageStyles from '../page.module.css';
-import Styles from './style.module.css';
+import BasicLayout from "../../layouts/BasicLayout";
+import PageStyles from "../page.module.css";
+import Styles from "./style.module.css";
+import FollowUserItem from "../../components/FollowUserItem";
 
-export default function UsersPage(){
+export default function UsersPage() {
     return (
         <BasicLayout>
-            <div className={`${Styles['search-container']} ${PageStyles['d-flex']} ${PageStyles['c-flex-axis0-center']}`}>
-                <div id="search-form" className={`${Styles['search-bar']} ${PageStyles['horizon-center-layout']}`}>
-                    <input id={Styles['search-input']} type="text" placeholder="Search..." name="search" value="" />
-                    <button id={`${Styles['search-btn']}`} className={`${'btn'} ${'c-border'}`}>Search</button>
+            <div
+                className={`${Styles["search-container"]} ${PageStyles["d-flex"]} ${PageStyles["c-flex-axis0-center"]}`}
+            >
+                <div
+                    id="search-form"
+                    className={`${Styles["search-bar"]} ${PageStyles["horizon-center-layout"]}`}
+                >
+                    <input
+                        id={Styles["search-input"]}
+                        type="text"
+                        placeholder="Search..."
+                        name="search"
+                        value=""
+                    />
+                    <button
+                        id={`${Styles["search-btn"]}`}
+                        className={`${"btn"} ${"c-border"}`}
+                    >
+                        Search
+                    </button>
                 </div>
             </div>
 
-            <div className={`${'flex-column'} ${'text-center'} ${PageStyles['d-flex']}`}>
+            <div
+                className={`${"flex-column"} ${"text-center"} ${
+                    PageStyles["d-flex"]
+                } ${"c-debug-red"}`}
+            >
+                <FollowUserItem id={1} />
+                <FollowUserItem id={2} />
+                <FollowUserItem id={3} />
             </div>
         </BasicLayout>
     );
