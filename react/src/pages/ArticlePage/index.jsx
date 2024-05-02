c240213b009f9f218222c5724da9184774b9917f
j32u4ukh <j32u4ukh@gmail.com>
2024-05-02 22:51:31 +0800

    feat: 調整頁面布局

--- react/src/pages/ArticlePage/index.jsx
+++ react/src/pages/ArticlePage/index.jsx
@@ -1,22 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import AlexImage from "../../../../src/data/Alex.png";
 import PageStyles from "../page.module.css";
 import Styles from "./styles.module.css";
+import axios from "axios";
+import { BASE_URL } from "../../utils";
+import { useSelector } from "react-redux";
+import { selectPersist } from "../../store/slice/persist";
 
 export default function ArticlePage() {
-    const messageBoxRef = useRef("");
     const navigate = useNavigate();
     const params = useParams();
     const articleId = params.id;
+    const messageBoxRef = useRef("");
+    const [article, setArticle] = useState({});
+    const persist = useSelector(selectPersist);
+    const jwt = persist.jwt;
+    const categories = persist.categories;
     let onClick = false;
-
-    const article = {
-        title: `文章${articleId}`,
-        author: `文章${articleId}作者`,
-        category: `類別${articleId}`,
-        content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    };
 
     useEffect(() => {
         // 檢查 id 是否為數字
@@ -24,18 +25,29 @@ export default function ArticlePage() {
             // 如果 id 不是數字，使用 history.replace() 重定向到 NotFoundPage
             navigate("/NotFound", { replace: true });
         }
+        axios
+            .get(`${BASE_URL}/articles/${articleId}`, {
+                headers: { authorization: `Bearer ${jwt}` },
+            })
+            .then((response) => {
+                const data = response.data;
+                console.log(`data: ${JSON.stringify(data)}`);
+                // data: {"id":1,"userId":1,"name":"Henry","image":"8Cma4wzXfNbRHg22xk2g8NU3V9aOKEr.png","title":"Article-0","category":1,"content":"This is content 0 hh","updatedAt":"2024-05-02T14:12:04.000Z"}
+                setArticle(data);
+            });
     }, []);
 
     function editRedirect() {
         navigate(`/articles/${articleId}/edit`);
     }
 
+    // TODO: 修正成 -> 點到留言區以外的地方，留言框縮小
     function onTextAreaHandler() {
         if (onClick) {
-            messageBoxRef.current.style.height = "";
+            messageBoxRef.current.style.height = "100px";
             onClick = false;
         } else {
-            messageBoxRef.current.style.height = "100px";
+            messageBoxRef.current.style.height = "120px";
             onClick = true;
         }
     }
@@ -52,29 +64,26 @@ export default function ArticlePage() {
     }
 
     return (
-        <>
-            {/* TODO container 與 留言區 用 div 再包覆*/}
-            <section className={PageStyles.container}>
-                <div className={Styles.content}>
-                    {/* TODO 置中 */}
-                    <h1 className={Styles.articleTitle}>{article.title}</h1>
-                    {/* TODO 置中 */}
-                    <h3>{article.author}</h3>
-                    {/* TODO 置中 */}
-                    <h3>{article.category}</h3>
-                    <div
-                        className={`${PageStyles["d-flex"]} ${PageStyles["c-flex-axis1-end"]} ${PageStyles["c-flex-axis2-column"]}`}
-                    >
-                        <article>{article.content}</article>
-                        <button id={Styles.editBtn} onClick={editRedirect}>
-                            <i
-                                className="fa-solid fa-pen-to-square"
-                                style={{ fontSize: "30px" }}
-                            ></i>
-                        </button>
-                    </div>
+        <section className={PageStyles.container}>
+            <div className={Styles.content}>
+                <h2 className={`${Styles.articleTitle} ${Styles.metadata}`}>
+                    {article.title}
+                </h2>
+                <h4 className={`${Styles.metadata}`}>{article.name}</h4>
+                <h4 className={`${Styles.metadata}`}>
+                    {categories[article.category]}
+                </h4>
+                <article>{article.content}</article>
+                <div className={PageStyles.applyBtns}>
+                    <button onClick={editRedirect}>
+                        {/* TODO: 換掉編輯按鈕圖示 */}
+                        <i
+                            className="fa-solid fa-pen-to-square"
+                            style={{ fontSize: "30px" }}
+                        ></i>
+                    </button>
                 </div>
-            </section>
+            </div>
 
             {/* 留言區 */}
             <div id={Styles.commentsContainer}>
@@ -84,22 +93,21 @@ export default function ArticlePage() {
                     </div>
                     <div className={Styles.commenterName}></div>
                 </div>
-                {/* TODO設定固定高度 Height */}
-                <form id={Styles.commentForm} onClick={onTextAreaHandler}>
-                    <textarea
-                        id={Styles.comment}
-                        ref={messageBoxRef}
-                        placeholder="請留下您的評論"
-                        required
-                    ></textarea>
-                </form>
-                <div className={Styles.commentControls}>
+                {/* TODO: 設定固定高度 Height */}
+                <textarea
+                    id={Styles.comment}
+                    ref={messageBoxRef}
+                    onClick={onTextAreaHandler}
+                    placeholder="請留下您的評論"
+                    required
+                ></textarea>
+                <div className={PageStyles.applyBtns}>
                     <button onClick={onCancelHandler}>取消</button>
                     <button>留言</button>
                 </div>
             </div>
             {/* 歷史留言區 (還要放留言者頭像及名字) */}
             <div className={Styles.commentList}></div>
-        </>
+        </section>
     );
 }
